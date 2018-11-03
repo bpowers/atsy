da1926b65a66c3e89c93aacafbd2cf9d86c0943f
Bobby Powers <bobbypowers@gmail.com>
2018-11-03 15:42:49 -0700

    use LTS firefox

--- example/comp_analysis_conf_speedometer.py
+++ example/comp_analysis_conf_speedometer.py
@@ -74,11 +74,11 @@ SETUP = {
         #     'path_filter': lambda x: 'obj-x86_64-pc-linux-gnu/dist/bin/firefox-bin' in x
         # },
         'Firefox': {
-            'binary': '/opt/firefox-61.0.1/bin/firefox',
+            'binary': '/opt/firefox-60.2.2/bin/firefox',
             #'binary': '/home/bpowers/src/firefox-57.0-nojemalloc/obj-x86_64-pc-linux-gnu/dist/bin/firefox-bin',
             # 'binary': '/home/bpowers/src/firefox-57.0/obj-x86_64-pc-linux-gnu/dist/bin/firefox-bin',
             'parent_filter': lambda x: 'firefox -content' not in x,
-            'path_filter': lambda x: '/opt/firefox-61.0.1' in x
+            'path_filter': lambda x: '/opt/firefox-60.2.2' in x
         },
     },
 }
